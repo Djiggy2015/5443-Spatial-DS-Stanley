0c8ffcc83bff257a9905f2b78113b39897f9b9f8
Djiggy2015 <49460397+Djiggy2015@users.noreply.github.com>
2020-10-05 03:17:09 -0700

    Update cityfilter.py

--- Assignments/Assignment3/cityfilter.py
+++ Assignments/Assignment3/cityfilter.py
@@ -1,33 +1,43 @@
-# This code was used to filter the coordinates from the major us cities 
-# geojson file.
+# This code was used to filter the coordinates from the major cities in the world.
+# The output of this will be worldcities.geojson, which is pretty large (40MB).
 
 import os
 import json
 import pprint
 import copy
 
-path = 'Assignment3/uscities.geojson'
+path = 'Assignment3/cities.json'
 if os.path.isfile(path):
-    with open(path, 'r') as f:
+    with open(path, 'r', encoding="utf-8") as f:
          data = f.read()
          data = json.loads(data)
 else:
     print("Error: Data was not found!")
 
 newdata = {"type":"FeatureCollection","features":[]}
-lonlat = {"type": "Feature", "geometry":{"type": "Point", "coordinates":[]}, "properties":{}}
+lonlat = {"type": "Feature", "geometry":{"type": "Point", "coordinates":[]}, "properties":{"name":"none"}}
 
-for feature in data['features']:
+for item in data:
+    # Get the longitude and latitude of each city, name as well.
+    lonlat["geometry"]["coordinates"].append(float(item["lng"]))
+    lonlat["geometry"]["coordinates"].append(float(item["lat"]))
+    lonlat["properties"]["name"] = item["name"]
 
-    lonlat["geometry"]["coordinates"].append(feature['geometry']['coordinates'][0])
-    lonlat["geometry"]["coordinates"].append(feature['geometry']['coordinates'][1])
+    # Make a copy that will be added to our dictionary of features.
     newlonlat = copy.deepcopy(lonlat)
     newdata["features"].append(newlonlat)
+
+    # Reset the dictionary so it will be ready to read the next city's info.
     lonlat["geometry"]["coordinates"] = []
+    lonlat["properties"]["name"] = " "
 
 
+# Make our geojson data. Indent 4 makes it look pretty.
 jdata = json.dumps(newdata, indent = 4)
 
-f = open("citieslonlat.geojson", "w")
+# Write our data out to a file.
+f = open("worldcities.geojson", "w")
 f.write(jdata)
 f.close()
+
+        
